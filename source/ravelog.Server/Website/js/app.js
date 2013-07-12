ee37f32df16885eb70a1892ab7b41cfc5c224438
smeyer <scott.meyer@firehost.com>
2013-07-12 13:06:39 -0500

    updated angular state implmentation

--- source/ravelog.Server/Website/js/app.js
+++ source/ravelog.Server/Website/js/app.js
@@ -1,49 +1,90 @@
 ﻿angular.module('ravelog', ['ravelog.controllers','ravelog.services', 'ui.state', 'ngGrid'])
 .config(['$routeProvider', '$stateProvider', function ($routeProvider, $stateProvider) {
 
-    $routeProvider
-        .when('/log/information',
-            {
-                templateUrl: 'partials/log-grid.html',
-                controller: 'InformationController'
-            })
-        .when('/log/warning',
-            {
-                templateUrl: 'partials/log-grid.html',
-                controller: 'WarningController'
-            })
-        .when('/log/error',
-            {
-                templateUrl: 'partials/log-grid.html',
-                controller: 'ErrorController'
-            })
-        .when('/log/trace',
-            {
-                templateUrl: 'partials/log-grid.html',
-                controller: 'TraceController'
-            })
-        .when('/test/submit',
-            {
-                templateUrl: 'partials/test.html',
-                controller: 'TestController'
-            })
-        .otherwise({ redirectTo: '/log/information' });
+    $routeProvider.when('/', 
+    {
+         redirectTo: '/log/information'
+    });
+
+    var logs = {
+        name: 'log',
+        url: "/log",
+        abstract: true,
+        templateUrl: 'partials/log-grid.html',
+        controller: 'LogController'
+    };
+
+    var information = {
+        name: 'log.information',
+        parent: logs,
+        url: '/information',
+        data: {
+            url: "http://localhost:8088/log/information"
+        },
+        onEnter: ['$state', function($state) {
+            if ($state.parent !== undefined) {
+                $state.parent.controller.refresh();
+            }
+        }]
+    };
+
+    var error = {
+        name: 'log.error',
+        parent: logs,
+        url: '/error',
+        data: {
+            url: "http://localhost:8088/log/error"
+        }
+        ,
+        onEnter: ['$state', function ($state) {
+            if ($state.parent !== undefined) {
+                $state.parent.controller.refresh();
+            }
+        }]
+    };
+    
+    var warning = {
+        name: 'log.warning',
+        parent: logs,
+        url: '/warning',
+        data: {
+            url: "http://localhost:8088/log/warning"
+        }
+        ,
+        onEnter: ['$state', function ($state) {
+            if ($state.parent !== undefined) {
+                $state.parent.controller.refresh();
+            }
+        }]
+    };
+    
+    var trace = {
+        name: 'log.trace',
+        parent: logs,
+        url: '/trace',
+        data: {
+            url: "http://localhost:8088/log/trace"
+        }
+        ,
+        onEnter: ['$state', function ($state) {
+            if ($state.parent !== undefined) {
+                $state.parent.controller.refresh();
+            }
+        }]
+    };
+
+    var test = {
+        name: 'test',
+        url: '/test',
+        templateUrl: 'partials/test.html',
+        controller: 'TestController'
+    };
 
     $stateProvider
-        .state('information', {
-            url: "/log/information",
-            templateUrl: "partials/log-grid.html"
-        })
-        .state('warning', {
-            url: "/log/warning",
-            templateUrl: "partials/log-grid.html"
-        })
-        .state('error', {
-            url: "/log/error",
-            templateUrl: "partials/log-grid.html"
-        })
-        .state('trace', {
-            url: "/log/trace",
-            templateUrl: "partials/log-grid.html"
-        });
+        .state(logs)
+        .state(information)
+        .state(warning)
+        .state(error)
+        .state(trace)
+        .state(test);
 }]);
--- source/ravelog.Server/Website/js/controllers.js
+++ source/ravelog.Server/Website/js/controllers.js
@@ -6,89 +6,44 @@
         };
 
     }])
-  .controller('InformationController', ['$scope','$http', function ($scope, $http) {
-      $scope.logData = [];
-      $scope.grid = {
-          data: 'logData',
-          multiSelect: false
-      };
+    .controller('LogController', ['$scope', '$http','$state', function ($scope, $http, $state) {
+        $scope.logData = [];
+        $scope.grid = {
+            data: 'logData',
+            multiSelect: false
+        };
+        
+        var refresh = function () {
+            $http({
+                url: $state.current.data.url,
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).success(function(data) {
+                $scope.logData = data;
+            });
+        };
 
-      $http({
-          url: 'http://localhost:8088/log/information',
-          method: "GET",
-          headers: {
-              "Content-Type": "application/json"
-          }
-      }).success(function (data) {
-          $scope.logData = data;
-      });
-  }])
-  .controller('WarningController', ['$scope', '$http', function ($scope, $http) {
-      $scope.logData = [];
-      $scope.grid = {
-          data: 'logData',
-          multiSelect: false
-      };
-
-      $http({
-          url: 'http://localhost:8088/log/warning',
-          method: "GET",
-          headers: {
-              "Content-Type": "application/json"
-          }
-      }).success(function (data) {
-          $scope.logData = data;
-      });
-  }])
-  .controller('ErrorController', ['$scope', '$http', function ($scope, $http) {
-      $scope.logData = [];
-      $scope.grid = {
-          data: 'logData',
-          multiSelect: false
-      };
-
-      $http({
-          url: 'http://localhost:8088/log/error',
-          method: "GET",
-          headers: {
-              "Content-Type": "application/json"
-          }
-      }).success(function (data) {
-          $scope.logData = data;
-      });
-  }])
-  .controller('TraceController', ['$scope', '$http', function ($scope, $http) {
-      $scope.logData = [];
-      $scope.grid = {
-          data: 'logData',
-          multiSelect: false
-      };
-
-      $http({
-          url: 'http://localhost:8088/log/trace',
-          method: "GET",
-          headers: {
-              "Content-Type": "application/json"
-          }
-      }).success(function (data) {
-          $scope.logData = data;
-      });
-  }])
+        $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+             refresh();
+        });
+    }])
   .controller('TestController', ['$scope', '$http', function ($scope, $http) {
 
-      $scope.url = 'http://localhost:8088/log/information';
-      $scope.payload = "{'dateCreated': '" + moment().format() + "', 'application': 'ravelog test', 'message':'This is a test message.'}";
+          $scope.url = 'http://localhost:8088/log/information';
+          $scope.payload = "{'dateCreated': '" + moment().format() + "', 'application': 'ravelog test', 'message':'This is a test message.'}";
 
-      $scope.send = function() {
-          $http({
-              url: $scope.url,
-              data: $scope.payload,
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json"
-              }
-          }).success(function(response) {
-              $scope.response = response;
-          });
-      };
+          $scope.send = function() {
+              $http({
+                  url: $scope.url,
+                  data: $scope.payload,
+                  method: "POST",
+                  headers: {
+                      "Content-Type": "application/json"
+                  }
+              }).success(function(response) {
+                  $scope.response = response;
+              });
+          };
   }]);
